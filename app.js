2870943c478c63abfff141b11ed3e57049696309
Milad64sh <m.shalikarian@gmail.com>
2022-08-14 09:24:57 -0700

    html, css, js

---
+++ app.js
@@ -0,0 +1,11 @@
+let ball = document.querySelector('.ball');
+let info = document.querySelector('.info');
+y = 1;
+gravity = 1;
+setInterval(function () {
+  if (y > 420) gravity = -gravity;
+  gravity += 1;
+  y += gravity;
+  ball.style.top = y + 'px';
+  info.innerText = `Y : ${y}, G : ${gravity}`;
+}, 1000);
